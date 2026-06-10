fc0c0e62b561b801a9a25b9fbcc2706cacd304d8
Animesh42u2 <mohapatraanimesh69@gmail.com>
2026-06-11 00:17:26 +0530

    jhagda

--- api/prokerala/[...path].ts
+++ api/prokerala/[...path].ts
@@ -1,10 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const path = (req.query.path as string[]).join('/')
-  const search = new URL(req.url!, 'http://localhost').search
+  const pathSegments = Array.isArray(req.query.path) 
+    ? req.query.path 
+    : [req.query.path as string]
+  
+  const path = pathSegments.join('/')
+  
+  // Strip query params that Vercel adds, rebuild clean search string
+  const urlObj = new URL(req.url!, 'http://localhost')
+  urlObj.searchParams.delete('path')
+  const search = urlObj.search
 
-  const url = `https://api.prokerala.com/v2/${path}${search}`
+  const url = `https://api.prokerala.com/${path}${search}`
+
+  console.log('Proxying to:', url)
 
   const response = await fetch(url, {
     headers: {
