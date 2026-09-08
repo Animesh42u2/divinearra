2d334fdbede0f273bb09bb6e3e48165b6773e181
Animesh42u2 <mohapatraanimesh69@gmail.com>
2026-09-08 23:59:50 +0530

    ADITYA  WEBSITE

--- src/data/Consultationsconfig.ts
+++ src/data/Consultationsconfig.ts
@@ -88,7 +88,7 @@ export const consultationsConfig: ConsultationConfig[] = [
         name: 'Aryabhata',
         tagline: 'Focused Guidance for Your Most Pressing Question',
         originalPrice: '₹1,249/-',
-        discountedPrice: '₹499',
+        discountedPrice: '₹1',
         features: [
           { label: '15 Minutes 1-On-1 Session', included: true },
           { label: 'Audio Consultation', included: true },
